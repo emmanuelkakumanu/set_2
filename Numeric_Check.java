dc0ba40ba4a8c7149b60006f304a1a3a33b3ced6
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-02 16:05:08 +0530

    Create Numeric_Check.java

---
+++ Numeric_Check.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Numeric_Check
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		String str=s.next();
+		int f=0;
+		for(int i=0;i<str.length();i++)
+		{
+			char c=str.charAt(i);
+			if(Character.isLetter(c))
+			f=1;
+		}
+		if(f==1)
+		System.out.println("no");
+		else
+		System.out.println("yes");
+	}
+}
