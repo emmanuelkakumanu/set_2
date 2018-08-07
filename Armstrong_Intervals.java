a429a2064ef9212369ef854a19286076869c4915
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-07 10:36:52 +0530

    print armstrong numbers between two intervals

---
+++ Armstrong_Intervals.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Armstrong_series
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int m=s.nextInt();
+		if(n>10 && n<m)
+		{
+		for(int i=n+1;i<m;i++)
+		{
+			int temp=i;
+			int arm=0;
+			while(temp!=0)
+			{
+				int r=temp%10;
+				arm=arm+(r*r*r);
+				temp=temp/10;
+				
+			}
+			if(arm==i)
+			System.out.print(arm+" ");
+		}
+		}
+		else
+		System.out.print("Invalid Inputs");
+
+	}
+}
